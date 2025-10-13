f11c39f0242e1e9b4c966ec06459e631be97a94c
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-10-13 11:54:16 +0530

    Updated header icons with separator

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,24 +2,23 @@ import Link from "next/link";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { SupportPopover } from "@/components/misc/support-popover";
 import GithubButton from "@/components/misc/github-button";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
 export function Header() {
   return (
     <header className="pb-2 flex justify-between items-center">
       <Link href="/">
-        <Button
-          variant="ghost"
-          size="icon-sm"
-           className="cursor-pointer"
-        >
+        <Button variant="ghost" size="icon-sm" className="cursor-pointer">
           <Image src="/favicon.ico" alt="Wallendar" width={50} height={50} />
         </Button>
       </Link>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1 h-5 ">
         <GithubButton />
+        <Separator orientation="vertical" />
         <SupportPopover />
+        <Separator orientation="vertical" />
         <ModeToggle />
       </div>
     </header>
--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -13,28 +13,39 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
+  const toggleTheme = React.useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark")
+  }, [resolvedTheme, setTheme])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="group/toggle extend-touch-target size-8"
+      onClick={toggleTheme}
+      title="Toggle theme"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="size-4.5"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M12 3l0 18" />
+        <path d="M12 9l4.65 -4.65" />
+        <path d="M12 14.3l7.37 -7.37" />
+        <path d="M12 19.6l8.85 -8.85" />
+      </svg>
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
