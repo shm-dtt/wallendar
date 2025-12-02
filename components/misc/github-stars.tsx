02a5a5044446d1e3a85fea2a24c34f7de29f9f8d
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-12-02 22:44:53 +0530

    fixed, reduced revalidate time

--- components/misc/github-stars.tsx
+++ components/misc/github-stars.tsx
@@ -2,24 +2,21 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Github } from "lucide-react";
 
-function GithubButton() {
-  async function StarsCount() {
-    const data = await fetch("https://api.github.com/repos/shm-dtt/wallendar", {
-      next: { revalidate: 86400 }, // Cache for 1 day (86400 seconds)
-    });
-    const json = await data.json();
-    return json.stargazers_count >= 1000
+async function GithubButton() {
+  const data = await fetch("https://api.github.com/repos/shm-dtt/wallendar", {
+    next: { revalidate: 43200 },
+  });
+  const json = await data.json();
+  const stars =
+    json.stargazers_count >= 1000
       ? `${(json.stargazers_count / 1000).toFixed(1)}k`
       : json.stargazers_count.toLocaleString();
-  }
 
   return (
     <Link href="https://github.com/shm-dtt/wallendar" target="_blank">
       <Button variant="ghost" className="cursor-pointer">
-        <Github aria-hidden="true" /> 
-        <span className="text-muted-foreground text-xs">
-          {StarsCount()}
-        </span>
+        <Github aria-hidden="true" />
+        <span className="text-muted-foreground text-xs">{stars}</span>
       </Button>
     </Link>
   );
